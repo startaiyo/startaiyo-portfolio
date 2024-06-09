a0da5b63c186dc78b223a5ea5f587d1fc2ddc754
startaiyo <startaiyo0104@gmail.com>
2024-06-09 21:24:57 +0900

    passing the build

--- src/app/components/myActivities.tsx
+++ src/app/components/myActivities.tsx
@@ -15,9 +15,9 @@ export const MyActivities = () => {
             className="bg-gray-500 p-4 rounded-lg text-white hover:bg-gray-300"
           >
             <div className="font-bold mb-4">Working at DeNA, implementing an iOS App of the medical information sharing service.</div>
-            <div>What I have done here:
+            <div>What I&apos;ve done here:
             <ul className="list-disc pl-4">
-              <li>Improving the onboarding document from the beginner's perspective</li>
+              <li>Improving the onboarding document from the beginner&apos;s perspective</li>
               <li>Making whole new feature by myself in one month</li>
               <li>Making the onboarding project of Swift</li>
               <li>Making a feature with outsourcing company to meet the criteria for legal requirement of App</li>
