ae1ca067a398fe5ddd7e171018212f255769dd61
startaiyo <startaiyo0104@gmail.com>
2024-06-23 08:12:13 +0900

    Add activities

--- src/app/components/myActivities.tsx
+++ src/app/components/myActivities.tsx
@@ -11,7 +11,7 @@ export const MyActivities = () => {
             Create Swift Onboarding project
           </a>
           <a
-            href="https://dena.com/"
+            href="https://www.allm.net/join/"
             className="bg-gray-500 p-4 rounded-lg text-white hover:bg-gray-300"
           >
             <div className="font-bold mb-4">Working at DeNA, implementing an iOS App of the medical information sharing service.</div>
@@ -19,10 +19,22 @@ export const MyActivities = () => {
             <ul className="list-disc pl-4">
               <li>Improving the onboarding document from the beginner&apos;s perspective</li>
               <li>Making whole new feature by myself in one month</li>
-              <li>Making the onboarding project of Swift</li>
+              <li>Decreasing the time of developing process by improving the flow, about 1/3 partially.</li>
               <li>Making a feature with outsourcing company to meet the criteria for legal requirement of App</li>
             </ul>
             </div>
+          </a>
+          <a
+            href="https://qiita.com/startaiyo"
+            className="bg-gray-500 p-4 rounded-lg text-white hover:bg-gray-300"
+          >
+            <div className="font-bold">Writing articles about Swift on Qiita</div>
+          </a>
+          <a
+            href="https://github.com/startaiyo/Eput2"
+            className="bg-gray-500 p-4 rounded-lg text-white hover:bg-gray-300"
+          >
+            <div className="font-bold">Create Eput App - an audio study application in iOS</div>
           </a>
         </div>
       </div>
